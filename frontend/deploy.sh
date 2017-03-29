9507f9d032ed2bb45f7f667fb559813731ca15f4
Andrei Papancea <andreipap10@yahoo.com>
2017-03-28 23:22:23 -0400

    cleaned up frontend deploy script

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
 CURRENT_PATH=$(pwd)
-AWS_PROFILE="concierge-demo"
-BUCKET_NAME="concierge.demo.mangata.io"
+
+AWS_PROFILE=""
+BUCKET_NAME="<YOUR-CONFIG-S3-BUCKET-NAME>"
+
+usage() {
+    echo "usage: $0"
+    printf "\t -p {AWS_PROFILE}\n" 1>&2; exit 1; }
+
+function check_profile()
+{
+    if [ -z $AWS_PROFILE ]; then
+        echo "error: missing aws profile"
+        usage
+        exit -1
+    fi
+}
 
 function upload_website_to_s3()
 {
@@ -22,6 +36,21 @@ function upload_website_to_s3()
 
   printf "\t[OK]\n"
 }
+
+# get flags
+while getopts "p:" o; do
+    case "${o}" in
+        p)
+            AWS_PROFILE=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+# check if the AWS_PROFILE variable is set
+check_profile
 
 # upload all the files to the S3 bucket
 upload_website_to_s3
